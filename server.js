a2e5a7e89a2f4a99cc1b54f16627eae2bc4636f4
Awayume <awayume2021@gmail.com>
2022-02-10 17:56:17 +0900

    change: 全体的に修正

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const http = require('http');
 const querystring = require('querystring');
-const discord = require('discord.js');
-const client = new discord.Client();
+const { Client } = require('discord.js');
+const { Intents } = require('./intents.json');
 
-http.createServer(function(req, res){
+const client = new Client({ intents: Intents.DEFAULT, partials: [ 'MESSAGE', 'CHANNEL' ] });
+
+http.createServer((req, res) =>{
   if (req.method == 'POST'){
-    var data = "";
-    req.on('data', function(chunk){
+    var data = '';
+    req.on('data', chunk =>{
       data += chunk;
     });
-    req.on('end', function(){
+    req.on('end', () =>{
       if(!data){
-        res.end("No post data");
+        res.end('No post data');
         return;
       }
       var dataObject = querystring.parse(data);
-      console.log("post:" + dataObject.type);
-      if(dataObject.type == "wake"){
-        console.log("Woke up in post");
+      console.log('post:' + dataObject.type);
+      if(dataObject.type == 'wake'){
+        console.log('Woke up in post');
         res.end();
         return;
       }
@@ -30,41 +32,88 @@ http.createServer(function(req, res){
   }
 }).listen(3000);
 
-client.on('ready', message =>{
-  console.log('Bot準備完了～');
-  client.user.setPresence({ activity: { name: 'げーむ' } });
+client.on('ready', async () =>{
+  console.table({
+    'Client user': client.user.tag,
+    'Client user ID': client.user.id,
+    'BOT': client.user.bot
+  });
+  await client.user.setPresence({ activities: [{ name: 'Your game name' }], status: 'online' });
+  console.info('Ready');
 });
 
-client.on('message', message =>{
-  if (message.author.id == client.user.id || message.author.bot){
-    return;
-  }
-  if(message.isMemberMentioned(client.user)){
-    sendReply(message, "呼びましたか？");
+client.on('messageCreate', async message =>{
+  // message.guildやmessage.channelなどが空の場合があるので注意
+  messageReceiveLog(message);
+  if (message.author.bot) return;
+  if(message.mentions.has(client.user, { ignoreEveryone: true })){
+    await sendReply(message, '呼びましたか？');
     return;
   }
   if (message.content.match(/にゃ～ん|にゃーん/)){
-    let text = "にゃ～ん";
-    sendMsg(message.channel.id, text);
+    await sendMsg(message, 'にゃ～ん');
     return;
   }
 });
 
 if(process.env.DISCORD_BOT_TOKEN == undefined){
- console.log('DISCORD_BOT_TOKENが設定されていません。');
- process.exit(0);
+  throw new Error('Environment variable "DISCORD_BOT_TOKEN" is not set');
 }
 
-client.login( process.env.DISCORD_BOT_TOKEN );
+client.login(process.env.DISCORD_BOT_TOKEN)
+  .then(() => console.info('Logged in to Discord'))
+  .catch(err =>{
+    console.error('Failed to log in to Discord\n' + err);
+    process.exit(1);
+  });
 
-function sendReply(message, text){
-  message.reply(text)
-    .then(console.log("リプライ送信: " + text))
+const messageReceiveLog = message =>{
+  if (message.author.id === client.user.id) return;
+  let log = {
+    'Message': message.content,
+    'Message ID': message.id,
+    'User': message.author.tag,
+    'User ID': message.author.id,
+    'BOT': message.author.bot,
+  }
+  if (!(message.channel.type === 'DM')){
+    log['Guild'] = message.guild.name
+    log['Guild ID'] = message.guild.id
+    log['Channel'] = message.channel.name
+    log['Channel ID'] = message.channel.id
+  }
+  else{
+    log['DM'] = true
+  }
+  console.group('Received a message');
+  console.table(log);
+  console.groupEnd();
+}
+
+const sendReply = async (message, text, options={}) =>{
+  await message.reply(text, options)
+    .then(msg =>{
+      console.group('Sent a reply message')
+      console.table({
+        'Message': text,
+        'Options': JSON.stringify(options),
+        'Message ID': msg.id
+      });
+      console.groupEnd();
+    })
     .catch(console.error);
 }
 
-function sendMsg(channelId, text, option={}){
-  client.channels.get(channelId).send(text, option)
-    .then(console.log("メッセージ送信: " + text + JSON.stringify(option)))
+const sendMsg = async (message, text, options={}) =>{
+  await message.channel.send(text, options)
+    .then(msg =>{
+      console.group('Sent a message');
+      console.table({
+        'Message': text,
+        'Options': JSON.stringify(options),
+        'Message ID': msg.id
+      });
+      console.groupEnd();
+    })
     .catch(console.error);
 }
